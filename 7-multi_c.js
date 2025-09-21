ee8e36aae9f4437e942fa451e7767d4e48086957
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-21 23:02:25 +0300

    first project

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,11 @@
-// Get the first argument
+// Get first argument and convert to integer
 const x = parseInt(process.argv[2]);
 
-// Check if x is a valid positive number
-if (isNaN(x) || x < 1) {
-    console.log("Missing number of occurrences");
-} else {
-    // Loop to print "C is fun" x times
+// Only proceed if x is a valid positive number
+if (!isNaN(x) && x > 0) {
     for (let i = 0; i < x; i++) {
         console.log("C is fun");
     }
+} else {
+    console.log("Missing number of occurrences");
 }
